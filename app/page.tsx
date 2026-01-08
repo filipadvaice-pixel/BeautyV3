9d45a41fca812ab1bc845c9931beb43f5ed1af07
filipadvaice-pixe <filipadvaice@gmail.com>
2026-01-08 19:01:30 +0300

    Rename header to MainNavbar to fix case sensitivity

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Services from '@/components/services'
 import MapSection from '@/components/map-section'
 import Reviews from '@/components/reviews'
 import Footer from '@/components/footer'
-import Header from '@/components/siteheader'
+import Header from '@/components/MainNavbar'
 import CalculatorModal from '@/components/calculator-modal'
 
 export default function Home() {
